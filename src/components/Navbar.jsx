66945031e77c6d4c2e7265f5e95d30512b468425
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-03-07 19:23:17 -0500

    started navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import categories from "../categories"
+import {Link} from "react-router-dom"
+
+function Navbar() {
+    return (
+        <div>
+            <ul className = "flex max-w-sm flex-wrap items-center">
+                {/* map = iterating */}
+                {categories.map((category) => (
+                    <li key = {category.name}>
+                        <Link to = {category.path}>{category.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
